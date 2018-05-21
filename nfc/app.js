fe95103495f24e753063a5edb0c6a1e4cf13afbf
Markus Tacker <m@coderbyheart.com>
2018-05-21 20:59:24 +0200

    refactor: improve code

--- nfc/app.js
+++ nfc/app.js
@@ -1,63 +1,57 @@
 'use strict'
 
-/* global digitalWrite LED1 LED2 I2C1 B6 B7 */
+/* global digitalWrite LED1 LED2 I2C1 B6 B7 E */
 
-// Wifi
+const pn532 = require('PN532')
+const http = require('http')
 const wifi = require('EspruinoWiFi')
+
+// Config
+const SLACK_HOOK = '/services/XXX/YYY/ZZZ'
 const WIFI_NAME = 'changeme'
 const WIFI_PASSWORD = 'changeme'
 
-// Slack
-const SLACK_HOOK_PATH = '/services/xxx/xxx/xxx'
-
+// Wifi
 let blinkState = 0
-let connected = false
 let connectingInterval = setInterval(function () {
   blinkState = !blinkState
   digitalWrite(LED2, blinkState)
 }, 250)
 
-wifi.connect(WIFI_NAME, {password: WIFI_PASSWORD}, function (err) {
-  if (err) {
-    console.log('Wifi connection error', err)
-    setInterval(function () {
-      blinkState = !blinkState
-      digitalWrite(LED1, blinkState)
-    }, 250)
-    return
-  }
-  clearInterval(connectingInterval)
-  console.log('Connected to Wifi!')
-  connected = true
-  toggleLock()
+function connectToWifi (callback) {
+  wifi.connect(WIFI_NAME, {password: WIFI_PASSWORD}, function (err) {
+    if (err) {
+      console.log('Wifi connection error', err)
+      setInterval(function () {
+        blinkState = !blinkState
+        digitalWrite(LED1, blinkState)
+      }, 250)
+      return callback(err)
+    }
+    clearInterval(connectingInterval)
+    console.log('Connected to Wifi!')
+    callback()
+  })
+}
 
-  // NFC
-  I2C1.setup({scl: B6, sda: B7}) // NFC Reader
-
-  var nfc = require('PN532').connect(I2C1)
-
-  nfc.SAMConfig() // start listening
-  function waitForCard () {
-    // console.log("Waiting for card...");
-    nfc.findCards(function (card) {
-      const id = JSON.stringify(card)
-      if (id === '[4,227,23,119,179,170,0]') { // Nordic
-        let locked = toggleLock()
-        announce('Markus', locked)
-      } else if (id === '[4,51,125,71,221,250,0]') {
-        let locked = toggleLock()
-        announce('Ivan', locked)
-      } else {
-        console.log('Card not recognized:', id)
-      }
-    })
-  }
-
-  setInterval(waitForCard, 1000)
-})
+// NFC
+function waitForCard (nfc) {
+  // console.log("Waiting for card...");
+  nfc.findCards(function (card) {
+    const id = JSON.stringify(card)
+    if (id === '[4,227,23,119,179,170,0]') { // Nordic
+      let locked = toggleLock()
+      announce('Markus', locked)
+    } else if (id === '[4,51,125,71,221,250,0]') {
+      let locked = toggleLock()
+      announce('Ivan', locked)
+    } else {
+      console.log('Card not recognized:', id)
+    }
+  })
+}
 
 // Logic
-
 let locked = true
 
 function toggleLock () {
@@ -73,21 +67,17 @@ function toggleLock () {
   return locked
 }
 
-const http = require('http')
+// Slack
 
 function announce (who, locked) {
   const content = JSON.stringify({
     text: who + ' has ' + (locked ? 'left' : 'entered')
   })
   console.log(content)
-  if (!connected) {
-    console.log('Announce: not connected.')
-    return
-  }
   http.request({
     host: 'hooks.slack.com',
     port: '443',
-    path: SLACK_HOOK_PATH,
+    path: SLACK_HOOK,
     protocol: 'https:',
     method: 'POST',
     headers: {
@@ -97,3 +87,21 @@ function announce (who, locked) {
   })
     .end(content)
 }
+
+// Main
+function init () {
+  toggleLock()
+  connectToWifi(function (err) {
+    if (err) return
+    // NFC
+    I2C1.setup({scl: B6, sda: B7}) // NFC Reader
+    const nfc = pn532.connect(I2C1)
+
+    nfc.SAMConfig() // start listening
+    setInterval(function () { waitForCard(nfc) }, 1000)
+  })
+}
+
+E.on('init', init)
+
+init()
